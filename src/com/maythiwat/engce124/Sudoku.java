a3044ab0376ef284b795ca922d6e0005e78f87d5
Maythiwat Chomchuen <maythiwat.chom@gmail.com>
2025-07-09 12:38:21 +0700

    feat: sudoku complete

--- src/com/maythiwat/engce124/Sudoku.java
+++ src/com/maythiwat/engce124/Sudoku.java
@@ -2,96 +2,108 @@ package com.maythiwat.engce124;
 
 public class Sudoku {
     public static void main(String[] args) {
-        int[][][][] region = {
+        int[][][][] boardFixed = {
                 {
-                        {{5, 3, 4}, {6, 7, 2}, {1, 9, 8}},
-                        {{6, 7, 8}, {1, 9, 5}, {3, 4, 2}},
-                        {{9, 1, 2}, {3, 4, 8}, {5, 6, 7}}
+                        {{5, 3, 4}, {6, 7, 8}, {9, 1, 2}},
+                        {{1, 7, 2}, {1, 9, 5}, {3, 4, 8}}, //
+                        {{1, 9, 8}, {3, 4, 2}, {5, 6, 7}}
                 },
                 {
-                        {{8, 5, 9}, {4, 2, 6}, {7, 1, 3}},
-                        {{7, 6, 1}, {8, 5, 3}, {9, 2, 4}},
-                        {{4, 2, 3}, {7, 9, 1}, {8, 5, 6}},
+                        {{8, 5, 9}, {7, 6, 1}, {4, 2, 3}},
+                        {{4, 2, 6}, {8, 5, 3}, {7, 9, 1}},
+                        {{7, 1, 3}, {9, 2, 4}, {8, 5, 6}}  //
                 },
                 {
-                        {{9, 6, 1}, {2, 8, 7}, {3, 4, 5}},
-                        {{5, 3, 7}, {4, 1, 9}, {2, 8, 6}},
-                        {{2, 8, 4}, {6, 3, 5}, {1, 7, 9}},
+                        {{9, 6, 1}, {5, 3, 7}, {2, 8, 4}},
+                        {{2, 8, 7}, {4, 1, 9}, {6, 3, 5}},
+                        {{3, 4, 5}, {2, 8, 6}, {1, 7, 9}}
                 }
         };
 
-        int[][][][] input = {
+        int[][][][] boardInput = {
                 {
-                        {{5, 3, 0}, {0, 0, 0}, {0, 9, 8}},
-                        {{0, 7, 0}, {1, 9, 5}, {0, 0, 0}},
-                        {{0, 0, 0}, {0, 0, 0}, {0, 6, 0}}
+                        {{5, 3, 0}, {0, 7, 0}, {0, 0, 0}},
+                        {{0, 0, 0}, {1, 9, 5}, {0, 0, 0}},
+                        {{0, 9, 8}, {0, 0, 0}, {0, 6, 0}},
                 },
                 {
-                        {{8, 0, 0}, {4, 0, 0}, {7, 0, 0}},
-                        {{0, 6, 0}, {8, 0, 0}, {3, 0, 2}}, //
-                        {{0, 0, 3}, {0, 0, 1}, {0, 0, 6}},
+                        {{8, 0, 0}, {0, 6, 0}, {0, 0, 3}},
+                        {{4, 0, 0}, {8, 0, 0}, {0, 0, 1}},
+                        {{7, 0, 0}, {3, 0, 2}, {0, 6, 0}},
                 },
                 {
-                        {{0, 6, 0}, {0, 0, 0}, {0, 0, 0}},
-                        {{0, 0, 0}, {4, 1, 9}, {0, 8, 0}},
-                        {{2, 8, 0}, {0, 0, 5}, {0, 7, 9}},
+                        {{0, 6, 0}, {0, 0, 0}, {2, 8, 0}},
+                        {{0, 0, 0}, {4, 1, 9}, {0, 0, 5}},
+                        {{0, 0, 0}, {0, 0, 8}, {7, 9, 0}}
                 }
         };
 
-        // printRegion(region, 1, 0);
+        // fillData(boardFixed, boardInput);
+
+        printBoard(boardFixed);
+        processSudoku(boardFixed);
+    }
+
+    public static void fillData(int[][][][] fixed, int[][][][] input) {
+        printBoard(input);
+        System.out.println();
 
         for (int r = 0; r < 3; r++) {
             for (int c = 0; c < 3; c++) {
-                printRegion(region, r, c);
+                for (int i = 0; i < 3; i++) {
+                    for (int j = 0; j < 3; j++) {
+                        if (input[r][c][i][j] == 0) {
+                            input[r][c][i][j] = fixed[r][c][i][j];
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    public static void processSudoku(int[][][][] board) {
+        for (int r = 0; r < 3; r++) {
+            if (!checkRow(board, r)) {
+                System.out.printf("\nLose! @ Row [%d]\n", r);
+                return;
             }
         }
 
-        System.out.println("WIN!!!");
+        for (int c = 0; c < 3; c++) {
+            if (!checkCol(board, c)) {
+                System.out.printf("\nLose! @ Column [%d]\n", c);
+                return;
+            }
+        }
+
+        for (int r = 0; r < 3; r++) {
+            for (int c = 0; c < 3; c++) {
+                if (!checkRegion(board, r, c)) {
+                    System.out.printf("\nLose! @ Region [%d,%d]\n", r, c);
+                    return;
+                }
+            }
+        }
+
+        System.out.println("\nCongratulations, You won!");
     }
 
-    public static void printRegion(int[][][][] region, int row, int col) {
-        StringBuilder seq = new StringBuilder();
+    public static void printBoard(int[][][][] board) {
+        for (int r = 0; r < 3; r++) {
+            for (int c = 0; c < 3; c++) {
+                printRegion(board, r, c);
+            }
+        }
+    }
+
+    public static void printRegion(int[][][][] board, int row, int col) {
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
-                System.out.print(region[row][col][i][j]);
-                seq.append(region[row][col][i][j]);
+                System.out.print(board[row][col][i][j]);
+                System.out.print(" ");
             }
-            System.out.println();
         }
-        // System.out.println(checkDuplicate(seq));
-
-        if (isDuplicate(seq.toString())) {
-            System.out.println("LOSE!!!");
-            System.exit(0);
-        }
-    }
-
-    public static void printRow(int[][][][] region, int row) {
-        for (int rs = 0; rs < 3; rs++) { // row select
-            StringBuilder seq = new StringBuilder();
-            for (int i = 0; i < 3; i++) {
-                for (int j = 0; j < 3; j++) {
-                    System.out.print(region[row][i][rs][j]);
-                    seq.append(region[row][i][rs][j]);
-                }
-            }
-            System.out.println();
-            System.out.println(isDuplicate(seq.toString()));
-        }
-    }
-
-    public static void printCol(int[][][][] region, int c) {
-        for (int cs = 0; cs < 3; cs++) { // col select
-            StringBuilder seq = new StringBuilder();
-            for(int r = 0; r < 3; r++) { // row
-                for (int j = 0; j < 3; j++) {
-                    System.out.print(region[r][c][j][cs]);
-                    seq.append(region[r][c][j][cs]);
-                }
-            }
-            System.out.println();
-            System.out.println(isDuplicate(seq.toString()));
-        }
+        System.out.println();
     }
 
     public static boolean isDuplicate(String str) {
@@ -106,5 +118,56 @@ public class Sudoku {
             }
         }
         return dupe;
+    }
+
+    public static boolean checkRegion(int[][][][] board, int row, int col) {
+        StringBuilder seq = new StringBuilder();
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                seq.append(board[row][col][i][j]);
+            }
+        }
+        System.out.println(seq.toString());
+        return !isDuplicate(seq.toString());
+    }
+
+    public static boolean checkRow(int[][][][] board, int row) {
+        for (int rs = 0; rs < 3; rs++) { // row select
+            StringBuilder seq = new StringBuilder();
+            for (int i = 0; i < 3; i++) {
+                for (int j = 0; j < 3; j++) {
+                    seq.append(board[row][rs][i][j]);
+
+                    System.out.println(row + "," + rs + "," + i + "," + j + " : " + seq);
+
+                    if (isDuplicate(seq.toString())) {
+                        System.out.println("\nError happens in: Block[" + row + "," + i + "] Sub[" + rs + "," + j + "] !!!! will exit ...");
+                        // return false;
+                        return false;
+                    }
+                }
+            }
+
+            if (isDuplicate(seq.toString())) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static boolean checkCol(int[][][][] board, int c) {
+        for (int cs = 0; cs < 3; cs++) { // col select
+            StringBuilder seq = new StringBuilder();
+            for (int r = 0; r < 3; r++) { // row
+                for (int j = 0; j < 3; j++) {
+                    seq.append(board[r][j][cs][c]);
+                }
+            }
+
+            if (isDuplicate(seq.toString())) {
+                return false;
+            }
+        }
+        return true;
     }
 }
